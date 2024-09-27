c46fbb8576cfb5259c1bd7bd43388ff5598fb990
Mayank <g.mayank82@yahoo.com>
2024-09-27 21:27:48 +0530

    Error fix : Username not set in validation

--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -10,7 +10,7 @@ import (
 )
 
 type RegisterInput struct {
-	Username string `json:"name" binding:"required"`
+	Username string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
