bff4f5afa30e24676cea30dfaa3c466f99a460e3
Mayank <g.mayank82@yahoo.com>
2024-10-24 14:07:48 +0530

    Added APIs for Income and distributed it properly

--- backend/main.go
+++ backend/main.go
@@ -19,26 +19,36 @@ func main() {
 	router := gin.Default()
 	router.Use(middleware.LoggerMiddleware())
 
-	// Home Apis
+	// Home APIs
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "Ok"})
 	})
 
-	// User apis
+	// User APIs
 	user := router.Group("/user")
 	user.POST("/register", controllers.RegisterUser)
 	user.POST("/login", controllers.LoginUser)
 	user.POST("/google/login", controllers.GoogleLogin)
 
-	// Protected APIs
-	protected := router.Group("/admin")
-	protected.Use(middleware.AuthMiddleware())
-	protected.GET("/current", controllers.CurrentUser)
-	protected.GET("/categories", controllers.ListAllCategories)
-	protected.POST("/category/new", controllers.AddNewCategory)
-	protected.GET("/expenses", controllers.ListAllExpenses)
-	protected.POST("/expense/new", controllers.AddNewExpense)
-	protected.DELETE("/expense", controllers.SoftDeleteExpense)
+	// Admin APIs
+	admin := router.Group("/admin")
+	admin.Use(middleware.AuthMiddleware())
+	admin.GET("/current", controllers.CurrentUser)
+
+	// Process APIs
+	process := router.Group("/process")
+	process.Use(middleware.AuthMiddleware())
+
+	process.GET("/categories", controllers.ListAllCategories)
+	process.POST("/category/new", controllers.AddNewCategory)
+
+	process.GET("/expenses", controllers.ListAllExpenses)
+	process.POST("/expense/new", controllers.AddNewExpense)
+	process.DELETE("/expense", controllers.SoftDeleteExpense)
+
+	process.GET("/incomes", controllers.ListAllIncomes)
+	process.POST("/income/new", controllers.AddNewIncome)
+	process.DELETE("/incomes", controllers.SoftDeleteIncome)
 
 	router.Run(":8000")
 }
