885f637b2c5bc11a3ce2b57050513504a6a24235
Mayank <g.mayank82@yahoo.com>
2024-10-24 14:09:59 +0530

    UUID + Total expense query

--- backend/models/expense.go
+++ backend/models/expense.go
@@ -4,15 +4,16 @@ import (
 	"log"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 )
 
 type Expense struct {
 	bun.BaseModel `bun:"table:expenses"`
 
-	ID          int64     `bun:"id,pk,autoincrement" json:"id"`
-	UserID      int64     `bun:"user_id,notnull" json:"user_id"` // Foreign key for User
-	CategoryID  int64     `bun:"category_id,notnull" json:"-"`   // Foreign key for Category
+	ID          uuid.UUID `bun:"type:uuid,pk,default:uuid_generate_v4()" json:"id"`
+	UserID      uuid.UUID `bun:"type:uuid,notnull" json:"user_id"` // Foreign key for User
+	CategoryID  uuid.UUID `bun:"type:uuid,notnull" json:"-"`       // Foreign key for Category
 	Amount      float64   `bun:"amount,notnull" json:"amount"`
 	Description string    `bun:"description,nullzero" json:"description"`
 	ExpenseDate time.Time `bun:"expense_date,notnull" json:"expense_date"`
@@ -36,13 +37,15 @@ func (expense *Expense) SaveExpense() (*Expense, error) {
 	_, err := DB.NewInsert().Model(expense).Exec(ctx)
 
 	if err != nil {
+		log.Println(err)
 		return &Expense{}, err
 	}
 	return expense, nil
 }
 
-func FilterExpenseByMonthAndYear(user_id int, month string, year string) ([]Expense, error) {
+func FilterExpenseByMonthAndYear(user_id uuid.UUID, month string, year string) ([]Expense, float64, error) {
 	var expenses []Expense
+	var totalExpenses float64
 
 	query := DB.NewSelect().Model(&expenses).Relation("Category").
 		Where("expense.user_id = ?", user_id).
@@ -56,5 +59,27 @@ func FilterExpenseByMonthAndYear(user_id int, month string, year string) ([]Expe
 	}
 
 	err := query.Scan(ctx)
-	return expenses, err
+	if err != nil {
+		log.Println(err)
+		return nil, 0, err
+	}
+
+	sumQuery := DB.NewSelect().Model(&Expense{}).
+		ColumnExpr("COALESCE(SUM(expense.amount), 0)").
+		Where("expense.user_id = ?", user_id)
+
+	if month != "" && year != "" {
+		sumQuery = sumQuery.Where("EXTRACT(YEAR FROM expense.expense_date) = ?", year).
+			Where("EXTRACT(MONTH FROM expense.expense_date) = ?", month)
+	} else if month == "" && year != "" {
+		sumQuery = sumQuery.Where("EXTRACT(YEAR FROM expense.expense_date) = ?", year)
+	}
+
+	err = sumQuery.Scan(ctx, &totalExpenses)
+	if err != nil {
+		log.Println(err)
+		return nil, 0, err
+	}
+
+	return expenses, totalExpenses, err
 }
